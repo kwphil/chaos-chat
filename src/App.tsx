c016fde8922cb7ab6371ffe96bad69de456aad57
kwphil <phillipsw1980@gmail.com>
2025-04-01 08:37:50 -0500

    Reworking...

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,17 @@
-import React, { useState } from 'react';
-import { Container, Box, AppBar, Toolbar, Typography } from '@mui/material';
-import MessageList from './components/MessageList';
-import MessageInput from './components/MessageInput';
+import { Box, Card, Typography } from '@mui/material';
 
-const App: React.FC = () => {
-  const [ messages, setMessages ] = useState([]);
-
-  const handleSendMessage = (message: string) => {
-    const newMessage = {
-      text: message,
-      sender: 'You',
-      timestamp: new Date().toLocaleTimeString(),
-    };
-    
-    setMessages([ ...messages, newMessage ]);
-  };
-
+const App = () => {
   return (
-    <Box sx= {{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
-      <AppBar position="sticky">
-        <Toolbar>
-          <Typography variant="h6">Chat</Typography>
-        </Toolbar>
-      </AppBar>
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Card variant="outlined">
+        <Typography variant="h4">
+          Lorem Ipsum
+        </Typography>
 
-      <Container sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-        <MessageList messages={ messages } />
-        <MessageList onSendMessage={ handleSendMessage } />
-      </Container>
+        <Typography>
+          
+        </Typography>
+      </Card>
     </Box>
   );
 };
